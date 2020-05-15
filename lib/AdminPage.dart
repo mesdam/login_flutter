434895c7e9f3fc3f7c6a2e18e5675e69be8d06cf
MesiDamayanti <mesidama54@gmail.com>
2020-05-15 19:50:43 +0800

    login

---
+++ lib/AdminPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatelessWidget {
+  AdminPage({this.username});
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome Admin"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Hallo $username',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          RaisedButton(
+              child: Text("LogOut"),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/MyHomePage');
+              }),
+        ],
+      ),
+    );
+  }
+}
